5e25f385e7326dafa8fae78020ad0019b9ef8405
jangdongguk <jangdong456@gmail.com>
2024-06-11 20:41:32 +0900

    test 삭제

--- src/com/jang/s1/StackStudy.java
+++ src/com/jang/s1/StackStudy.java
@@ -2,18 +2,18 @@ package com.jang.s1;
 
 public class StackStudy {
 
-	public test[] add(String[] ar) {
+	public void add(String[] ar) {
 		// 매개변수로 값을 받은 데이터 배열의  하나의 임의 데이터를 추가 
 		// 제일 앞에 추가된다.
 		// 추가된 배열을 리턴
 		for(int i=0; i<ar.length; i++) {
 			String test = new String("");
 			test = test+ar[i];
-			return test;
+			
 			
 		}
 		
-		return; // int [] 배열타입으로 리턴 해야함
+		 // int [] 배열타입으로 리턴 해야함
 	}
 	
 	public int [] remove(int [] ar) {
